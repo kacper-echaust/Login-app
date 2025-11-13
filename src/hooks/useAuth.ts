f33f240d637b0c8fe8f75500a7d4b2087bc32b53
kacper-echaust <k9echaust@gmail.com>
2025-11-13 17:33:22 +0100

    create hook which use login and register functions

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,61 @@
+import type { User } from "@/types";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const useAuth = () => {
+  const { setIsAuth } = useContext(AuthContext);
+  const [users, setUsers] = useState<User[]>();
+  const [error, setError] = useState<string>("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+
+    const users2 = querySnapshot.docs.map((doc) => ({
+      ...(doc.data() as User),
+    }));
+    setUsers(users2);
+  };
+  const loginUser = (data: User) => {
+    const existingUser = users?.find(
+      (user) =>
+        user.password === data.password &&
+        user.username.toLowerCase() === data.username.toLowerCase()
+    );
+    if (existingUser) {
+      setIsAuth(true);
+      navigate("/logged");
+      setError("");
+      return;
+    } else {
+      setIsAuth(false);
+      setError("Incorrect username or password");
+      return;
+    }
+  };
+  const registerUser = async (data: User) => {
+    const checkUsername = users?.find(
+      (user) => user.username.toLowerCase() === data.username.toLowerCase()
+    );
+    if (checkUsername) {
+      setError("Username is already exist");
+      return;
+    }
+    await addDoc(collection(db, "users"), {
+      ...data,
+    });
+    await getUsers();
+    navigate("/login");
+  };
+
+  return { loginUser, registerUser, error };
+};
+
+export { useAuth };
