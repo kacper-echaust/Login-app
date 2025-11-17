0ead7b489dc4635cb4316865a320095aa64bdf5c
kacper-echaust <k9echaust@gmail.com>
2025-11-17 18:58:08 +0100

    change types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,11 @@
 export type User = {
   username: string;
   password: string;
+  id: string;
+};
+export type Login = Omit<User, "id">;
+export type Register = {
+  username: string;
+  password: string;
+  terms: boolean;
 };
