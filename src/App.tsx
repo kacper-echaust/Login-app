c7d88710864cef8833e67d8ac098bff171b25249
kacper-echaust <k9echaust@gmail.com>
2025-11-17 19:01:25 +0100

    use components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { FormContainer } from "./components/FormContainer";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
+import { Logged } from "./components/Logged";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <AuthContextProvider>
       <BrowserRouter>
         <FormContainer />
+        <Routes>
+          <Route
+            path="/logged"
+            element={
+              <ProtectedRoute>
+                <Logged />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </AuthContextProvider>
   );
