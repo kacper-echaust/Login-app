1bb0bdbd9acd1266d0f8af92e5da8d7f5ca95f42
kacper-echaust <k9echaust@gmail.com>
2025-11-17 19:01:14 +0100

    add component logged

---
+++ src/components/Logged.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useAuth } from "@/hooks/useAuth";
+
+const Logged = () => {
+  const { logOut } = useAuth();
+  return (
+    <div>
+      <h3>You are Logged!</h3>
+      <Link to="/">
+        <Button onClick={logOut}>Log Out</Button>
+      </Link>
+    </div>
+  );
+};
+
+export { Logged };
