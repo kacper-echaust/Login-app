7f2726b48e93fed423862df2224ab26ae8cefb3a
kacper-echaust <k9echaust@gmail.com>
2025-11-18 18:04:31 +0100

    remove all routes to container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import { FormContainer } from "./components/FormContainer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
-import { Logged } from "./components/Logged";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { Home } from "./components/Home";
+import { RoutesContainer } from "./components/FormContainer";
 
 const App = () => {
   return (
     <AuthContextProvider>
       <BrowserRouter>
-        <FormContainer />
-        <Routes>
-          <Route
-            path="/logged"
-            element={
-              <ProtectedRoute>
-                <Logged />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <RoutesContainer />
       </BrowserRouter>
     </AuthContextProvider>
   );
--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -1,14 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import { LoginForm } from "./Form/LoginForm";
 import { RegisterForm } from "./Form/RegisterForm";
+import { ProtectedRoute } from "./ProtectedRoute";
+import { Logged } from "./Logged";
+import { Home } from "./Home";
 
-const FormContainer = () => {
+const RoutesContainer = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
+      <Route
+        path="/logged"
+        element={
+          <ProtectedRoute>
+            <Logged />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
 
-export { FormContainer };
+export { RoutesContainer };
